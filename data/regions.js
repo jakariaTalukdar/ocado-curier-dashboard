f2a7789365153f21b89b2674f5fd5d0c901c8e77
fxjakaria04 <freelancerx2023@gmail.com>
2025-03-15 17:25:59 +0600

    data added

--- data/regions.js
+++ data/regions.js
@@ -1,20 +1,24 @@
 export const regions = [
     {
+      id:1,
       name: "Dhaka",
       slug: "dhaka",
       image: "/images/chef.webp",
     },
     {
+      id:2,
       name: "Chittagong",
       slug: "chittagong",
       image: "/images/banner3.jpg",
     },
     {
+      id:3,
       name: "Sylhet",
       slug: "sylhet",
       image: "/images/banner2.jpg",
     },
     {
+      id:4,
       name: "Rajshahi",
       slug: "rajshahi",
       image: "/images/banner.jpg",
