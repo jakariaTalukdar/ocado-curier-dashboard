51842bedad31a4c91bd738b8544dd79f26148932
fxjakaria04 <freelancerx2023@gmail.com>
2025-03-18 14:42:04 +0600

    restaurents api added

---
+++ data/restaurents.js
@@ -0,0 +1,52 @@
+export const restaurants = [
+  {
+      regionId: 1, // Dhaka
+      restaurants: [
+          { id: 101, name: "Dhaka Dine", location: "Gulshan, Dhaka", exactLocation: "Road 12, Gulshan-2, Dhaka", image: "/images/dhaka1.jpg", rating: 4.5, ratingCount: 320 },
+          { id: 102, name: "Biryani House", location: "Dhanmondi, Dhaka", exactLocation: "House 5, Road 3, Dhanmondi, Dhaka", image: "/images/dhaka2.jpg", rating: 4.7, ratingCount: 450 },
+          { id: 103, name: "Spicy Grill", location: "Uttara, Dhaka", exactLocation: "Sector 13, Uttara, Dhaka", image: "/images/dhaka3.jpg", rating: 4.3, ratingCount: 210 },
+          { id: 104, name: "Royal Cuisine", location: "Banani, Dhaka", exactLocation: "Road 11, Banani, Dhaka", image: "/images/dhaka4.jpg", rating: 4.6, ratingCount: 380 },
+          { id: 105, name: "Taste Buds", location: "Mirpur, Dhaka", exactLocation: "Mirpur 10, Dhaka", image: "/images/dhaka5.jpg", rating: 4.2, ratingCount: 150 }
+      ]
+  },
+  {
+      regionId: 2, // Chittagong
+      restaurants: [
+          { id: 201, name: "Seafood Delight", location: "GEC, Chittagong", exactLocation: "GEC Circle, Chittagong", image: "/images/ctg1.jpg", rating: 4.8, ratingCount: 500 },
+          { id: 202, name: "Port City Diner", location: "Agrabad, Chittagong", exactLocation: "SK Mujib Road, Agrabad, Chittagong", image: "/images/ctg2.jpg", rating: 4.4, ratingCount: 340 },
+          { id: 203, name: "Spice Harbor", location: "Pahartali, Chittagong", exactLocation: "Pahartali Bazar, Chittagong", image: "/images/ctg3.jpg", rating: 4.3, ratingCount: 270 },
+          { id: 204, name: "Mezban Feast", location: "Chawkbazar, Chittagong", exactLocation: "Chawkbazar, Chittagong", image: "/images/ctg4.jpg", rating: 4.6, ratingCount: 390 },
+          { id: 205, name: "Bay View Bistro", location: "Patenga, Chittagong", exactLocation: "Patenga Beach Road, Chittagong", image: "/images/ctg5.jpg", rating: 4.7, ratingCount: 410 }
+      ]
+  },
+  {
+      regionId: 3, // Sylhet
+      restaurants: [
+          { id: 301, name: "Sylhet Treats", location: "Zindabazar, Sylhet", exactLocation: "Zindabazar Road, Sylhet", image: "/images/sylhet1.jpg", rating: 4.5, ratingCount: 280 },
+          { id: 302, name: "Tea Valley Cafe", location: "Lalakhal, Sylhet", exactLocation: "Lalakhal Road, Sylhet", image: "/images/sylhet2.jpg", rating: 4.9, ratingCount: 320 },
+          { id: 303, name: "Hillside Bites", location: "Tamabil, Sylhet", exactLocation: "Tamabil Road, Sylhet", image: "/images/sylhet3.jpg", rating: 4.3, ratingCount: 180 },
+          { id: 304, name: "Shahi Morog Polao", location: "Ambarkhana, Sylhet", exactLocation: "Ambarkhana Main Road, Sylhet", image: "/images/sylhet4.jpg", rating: 4.6, ratingCount: 250 },
+          { id: 305, name: "Green View Restaurant", location: "Shahjalal Road, Sylhet", exactLocation: "Shahjalal Road, Sylhet", image: "/images/sylhet5.jpg", rating: 4.4, ratingCount: 200 }
+      ]
+  },
+  {
+      regionId: 4, // Rajshahi
+      restaurants: [
+          { id: 401, name: "Rajshahi Flavors", location: "Shaheb Bazar, Rajshahi", exactLocation: "Shaheb Bazar Road, Rajshahi", image: "/images/rajshahi1.jpg", rating: 4.5, ratingCount: 220 },
+          { id: 402, name: "Puthia Palace Dining", location: "Puthia, Rajshahi", exactLocation: "Puthia Upazila, Rajshahi", image: "/images/rajshahi2.jpg", rating: 4.6, ratingCount: 310 },
+          { id: 403, name: "Gourmet Rajshahi", location: "New Market, Rajshahi", exactLocation: "New Market Area, Rajshahi", image: "/images/rajshahi3.jpg", rating: 4.4, ratingCount: 180 },
+          { id: 404, name: "Mango Grove Cafe", location: "Binodpur, Rajshahi", exactLocation: "Binodpur Bazar, Rajshahi", image: "/images/rajshahi4.jpg", rating: 4.3, ratingCount: 150 },
+          { id: 405, name: "Padma Riverside Eatery", location: "Padma Garden, Rajshahi", exactLocation: "Padma Garden Road, Rajshahi", image: "/images/rajshahi5.jpg", rating: 4.7, ratingCount: 270 }
+      ]
+  },
+  {
+      regionId: 5, // Sirajganj Sadar
+      restaurants: [
+          { id: 501, name: "Jamuna River Dine", location: "Sirajganj Sadar", exactLocation: "Jamuna River Bank, Sirajganj", image: "/images/sirajganj1.jpg", rating: 4.6, ratingCount: 250 },
+          { id: 502, name: "Sadar Food Corner", location: "Sirajganj Sadar", exactLocation: "Main Road, Sirajganj Sadar", image: "/images/sirajganj2.jpg", rating: 4.4, ratingCount: 180 },
+          { id: 503, name: "Golden Spoon", location: "Sirajganj Sadar", exactLocation: "New Market, Sirajganj Sadar", image: "/images/sirajganj3.jpg", rating: 4.3, ratingCount: 190 },
+          { id: 504, name: "Mezban Sirajganj", location: "Sirajganj Sadar", exactLocation: "Bus Stand, Sirajganj Sadar", image: "/images/sirajganj4.jpg", rating: 4.5, ratingCount: 210 },
+          { id: 505, name: "Taste of Tradition", location: "Sirajganj Sadar", exactLocation: "Kazipur Road, Sirajganj Sadar", image: "/images/sirajganj5.jpg", rating: 4.7, ratingCount: 230 }
+      ]
+  }
+];
